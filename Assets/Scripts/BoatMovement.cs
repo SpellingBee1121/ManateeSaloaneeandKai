389f6afe38dfd8b1088e0cfd92adc7b1f9db8448
kaihall <49812013+kaihall@users.noreply.github.com>
2021-03-18 10:11:57 -0400

    Boat Movement Script
    Use:
    
    1. Add the script to a boat
    2. Add two empty GameObjects to the scene: One for the boat's starting position and one for its ending position. For now, their y and z should be the same but their x values should be different.
    3. Drag the GameObject for the starting position into the StartPos variable on the script the Unity Editor
    4. Drag the GameObject for the ending position into the EndPos variable
    5. Set the min/max speed (the boat will move forward by 1/10 this distance every frame)
    6. Test

---
+++ Assets/Scripts/BoatMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMovement : MonoBehaviour
+{
+	private float speed;
+	private System.Random random = new System.Random();
+	public int minSpeed = 1;
+	public int maxSpeed = 5;
+	public Transform startPos;
+	public Transform endPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startPos.position.x > endPos.position.x) {
+			transform.position = transform.position + (Vector3.left * speed);
+			if (transform.position.x < endPos.position.x)
+				Reset();
+		}
+		else {
+			transform.position = transform.position + (Vector3.right * speed);
+			if (transform.position.x > endPos.position.x)
+				Reset();
+		}
+    }
+	
+	private void Reset() {
+		transform.position = startPos.position;
+		speed = random.Next(minSpeed, maxSpeed) / 10f;
+	}
+}
