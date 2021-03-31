45602ee1d96cac827de909a7347198130b696f32
Saloanee <saloanee.labh19@ncf.edu>
2021-03-31 13:59:19 -0400

    boat and camera collision works

--- Assets/Scripts/ManateeSocial.cs
+++ Assets/Scripts/ManateeSocial.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ManateeSocial : MonoBehaviour
 {
+    private float movementSpeed = 5f;
     private GameObject heart;
     public NeedsManager needs;
 
     public AudioSource msound;
+    //public Transform manateestartPos;
+	//public Transform manateeendPos;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +27,11 @@ public class ManateeSocial : MonoBehaviour
         heart.SetActive(true);
         StartCoroutine("HeartStop");
         msound.Play();
+        
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(movementSpeed * Time.deltaTime, verticalInput * movementSpeed * Time.deltaTime, 0);
+
     }
 
     private IEnumerator HeartStop()
